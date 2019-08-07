3648b74ab09ed375c43f0b555829c5443a5438ff
jianguang.wang <572707836@qq.com>
2019-08-07 14:49:36 +0800

    为响应结果封装提供解决方案

---
+++ src/main/java/com/wangjg/framework/configuration/ReturnHandler.java
@@ -0,0 +1,47 @@
+package com.wangjg.framework.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author wangjg
+ * 2019-08-07
+ * <p>
+ * 对没有发生异常情况下接口响应数据结构进行同一封装（对异常的处理，
+ * 可以通过在 {@link com.wangjg.framework.controller.BaseController} 中添加异常处理方法处理）
+ */
+@Slf4j
+public class ReturnHandler extends RequestResponseBodyMethodProcessor {
+
+    ReturnHandler(List<HttpMessageConverter<?>> converters) {
+        super(converters);
+    }
+
+    /**
+     * 该处理程序是否支持给定的方法返回类型(只有返回true才回去调用handleReturnValue)
+     */
+    @Override
+    public boolean supportsReturnType(MethodParameter methodParameter) {
+        return super.supportsReturnType(methodParameter);
+    }
+
+    /**
+     * 处理给定的返回值
+     * 通过向 ModelAndViewContainer 添加属性和设置视图或者
+     * 通过调用 ModelAndViewContainer.setRequestHandled(true) 来标识响应已经被直接处理(不再调用视图解析器)
+     * <p>
+     * 对响应数据 object 进行封装
+     */
+    @Override
+    public void handleReturnValue(Object o, MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest) throws IOException, HttpMediaTypeNotAcceptableException {
+        super.handleReturnValue(o, methodParameter, modelAndViewContainer, nativeWebRequest);
+    }
+}
---
+++ src/main/java/com/wangjg/framework/configuration/WebMvcConfiguration.java
@@ -0,0 +1,58 @@
+package com.wangjg.framework.configuration;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.HttpEntityMethodProcessor;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author wangjg
+ * 2019-08-07
+ *
+ * mvc 配置类
+ */
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer, InitializingBean {
+
+    @Resource
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    @Bean
+    public RequestResponseBodyMethodProcessor getReturnHandler() {
+        List<HttpMessageConverter<?>> messageConverters = requestMappingHandlerAdapter.getMessageConverters();
+        //初始化处理器
+        return new ReturnHandler(messageConverters);
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        final List<HandlerMethodReturnValueHandler> originalHandlers = new ArrayList<>(
+                requestMappingHandlerAdapter.getReturnValueHandlers());
+
+        // Add customed handler BEFORE the HttpEntityMethodProcessor to enable JsonReturnHandler
+        final int deferredPos = obtainValueHandlerPosition(originalHandlers);
+        originalHandlers.add(deferredPos - 1, getReturnHandler());
+        requestMappingHandlerAdapter.setReturnValueHandlers(originalHandlers);
+    }
+
+    private int obtainValueHandlerPosition(final List<HandlerMethodReturnValueHandler> originalHandlers) {
+        for (int i = 0; i < originalHandlers.size(); i++) {
+            final HandlerMethodReturnValueHandler valueHandler = originalHandlers.get(i);
+            if (HttpEntityMethodProcessor.class.isAssignableFrom(valueHandler.getClass())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+}
