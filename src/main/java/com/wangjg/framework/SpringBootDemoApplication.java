844a43c1eb00aa9ebac6dc712487f9b2c928abbd
jianguang.wang <572707836@qq.com>
2019-09-23 16:25:03 +0800

    解决未知bug

--- src/main/java/com/wangjg/framework/SpringBootDemoApplication.java
+++ src/main/java/com/wangjg/framework/SpringBootDemoApplication.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author wangjg
- * 2019-06-04
+ * @since 2019/9/23
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.wangjg.framework.mapper")
 public class SpringBootDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
-
 }
